318688f175acb134bf8bc303f548e7a47902d358
Tienho9 <114680051+Tienho9@users.noreply.github.com>
2023-10-24 11:59:27 +0700

    Delete BTTH05 directory

--- BTTH05/resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-@section('title','Welcome Page')
-@section('content')
-    <div class="container">
-        <h1 class="text-center text-success my-3">Quản Lý Thư Viện</h1>
-    </div>
-@endsection
--- BTTH05/storage/framework/views/85756913a626ed9f954c1216a012bf15.php
+++
@@ -1,8 +0,0 @@
-
-<?php $__env->startSection('title','Welcome Page'); ?>
-<?php $__env->startSection('content'); ?>
-    <div class="container">
-        <h1 class="text-center text-success my-3">Quản Lý Thư Viện</h1>
-    </div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\CN Web\ThucHanh\laravel-crud\resources\views/index.blade.php ENDPATH**/ ?>
